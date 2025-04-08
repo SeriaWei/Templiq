be7f1a4df135f8a172e7a450db0dfe563a46dda7
Wayne Wei <Wayne_Wei@Kingston.com.cn>
2025-04-08 16:10:27 +0800

    新增多个section的定义和模板文件，包括作品集、产品特性和创新设计展示，丰富了数据结构和展示逻辑。

--- src/tools/new-section.js
+++ src/tools/new-section.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
-const sectionName = Math.random().toString(36).substring(2, 8);
+const sectionName = new Date().getTime().toString(36).substring(2);
 const newSectionTemplate = `{% header %}
 <style>
 .section-${sectionName} {}
--- src/tools/new-section.ts
+++ src/tools/new-section.ts
@@ -1,7 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-
-const sectionName = Math.random().toString(36).substring(2, 8);
+const sectionName = new Date().getTime().toString(36).substring(2);
 const newSectionTemplate = 
 `{% header %}
 <style>
