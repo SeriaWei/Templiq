8f38f3e55eca53ef96398e5fdadce87b5bd46b1b
wayne <seriawei@outlook.com>
2025-04-13 16:54:32 +0800

    style: 修复模板字符串的缩进问题

--- src/tools/new-section.ts
+++ src/tools/new-section.ts
@@ -4,19 +4,19 @@ import path from 'path';
 export function createNewSection(customName?: string) {
     const sectionName = customName || new Date().getTime().toString(36).substring(2);
     const newSectionTemplate = 
-    `{% header %}
-    <style>
-    .section-${sectionName} {}
-    </style>
-    {% endheader %}
+`{% header %}
+<style>
+.section-${sectionName} {}
+</style>
+{% endheader %}
 
-    <section class="section-${sectionName}">
-    </section>
+<section class="section-${sectionName}">
+</section>
 
-    {% footer %}
-    <script type="text/javascript">
-    </script>
-    {% endfooter %}`;
+{% footer %}
+<script type="text/javascript">
+</script>
+{% endfooter %}`;
 
     const newSectionFile = path.join(__dirname, '..', 'templates', `section-${sectionName}.liquid`);
     fs.writeFileSync(newSectionFile, newSectionTemplate);
