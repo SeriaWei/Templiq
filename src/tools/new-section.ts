ebb7bc5591cac211a71db73d77548a6a735472a2
wayne <seriawei@outlook.com>
2025-04-27 20:26:31 +0800

    feat: 添加新的游玩区域模块及其相关资源
    添加了新的游玩区域模块，包括模板文件、数据文件、图片资源以及样式定义。该模块展示了多个游戏卡片，每个卡片包含图片、标题、图标和链接，用户可以通过该模块了解不同的游戏项目。

--- src/tools/new-section.ts
+++ src/tools/new-section.ts
@@ -23,11 +23,11 @@ export function createNewSection(customName?: string) {
     const newSectionFile = path.join(__dirname, '..', 'templates', `section-${sectionName}.liquid`);
     fs.writeFileSync(newSectionFile, newSectionTemplate);
 
-    const newDataFile = path.join(__dirname, '..', 'data', `section-${sectionName}.json`);
-    fs.writeFileSync(newDataFile, "{}");
+    // const newDataFile = path.join(__dirname, '..', 'data', `section-${sectionName}.json`);
+    // fs.writeFileSync(newDataFile, "{}");
 
-    const newDefFile = path.join(__dirname, '..', 'data', `section-${sectionName}.def.json`);
-    fs.writeFileSync(newDefFile, "{}");
+    // const newDefFile = path.join(__dirname, '..', 'data', `section-${sectionName}.def.json`);
+    // fs.writeFileSync(newDefFile, "{}");
 
     console.log(`New section ${sectionName} created at ${newSectionFile}`);
     return { sectionName, filePath: newSectionFile };
